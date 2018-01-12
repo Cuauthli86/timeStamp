91a522b91add32ef92546be13a3d57593a6e4fbc
Cuauhtli Campos <cc.mijangos@gmail.com>
2018-01-12 09:52:55 -0600

    This is my proposal to solve the timestamp project challeng from fcc. I think it can be improved by evaulating the natural string formmat recived and determin it is another type of express a date, e.g.  1 de julio de 2017

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var strftime = require('strftime');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/:timestring', function(req, res){
+//:Time is the variable to accces the string
+var timestring=req.params.timestring;
+res.json(timeStamp(timestring));
+});
+
+function timeStamp(timestring){
+var timeObject={
+    unix:null,
+    natural:null
+}
+
+var date;
+
+if(!isNaN (parseInt(timestring))){
+date=new Date(parseInt(timestring));
+
+}else {
+date= new Date(timestring);
+}
+
+if (!isNaN(date.getTime())){
+    timeObject.unix=date.getTime();
+    timeObject.natural=naturalDateConverter(date);
+}
+
+return timeObject;
+}
+
+function naturalDateConverter(date){
+
+var natural= strftime('%B %d, %Y', date);
+return natural;
+
+
+}
+app.listen(3000, function (){
+    console.log('Node is on');
+});
